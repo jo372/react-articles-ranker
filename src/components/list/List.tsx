5de0271a97acb78243436babd5f3df507c96a328
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-06-24 18:27:36 +0100

    a replacement for the article-list.js file. This contains ListType, ListProps and of course the List React component.

---
+++ src/components/list/List.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+// creating a list type which is just an enum with the string value as I can updated this whenever I should need to.
+enum ListType {
+    ORDERED = "ordered",
+    UNORDERED = "unordered"
+}
+
+// creating the list properties which should contain the type (ListType) and the items you want to add string[]
+interface ListProps {
+    type: ListType,
+    items: string[]
+}
+
+class List extends React.Component<ListProps> {
+    // assigning type on list to be the same as the enum created above so users who import List can just use List.type.UNORDERED
+    static type = ListType;
+    // setting the default to unordered as it the most common list type used with not item attached.
+    static defaultProps = {
+        type: ListType.UNORDERED,
+        items: []
+    }
+    render() {
+        const { type, items } = this.props;
+
+        // looping through and generating a unique key for the list items.
+        const listElements = items.map((itemText, index) => <li key={`${itemText}_${new Date().getTime()}_${index}`}>{ itemText }</li>);
+        // depending on the ListType depends on what the parent container will be.
+        switch(type) {
+            case ListType.ORDERED:
+                return <ol>{listElements}</ol>
+                break;
+            case ListType.UNORDERED:
+                return <ul>{listElements}</ul>
+                break;
+        }
+    }
+}
+
+export default List;
